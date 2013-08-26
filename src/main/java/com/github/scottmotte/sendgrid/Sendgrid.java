1b6b8f0438e64d8eed2f38db2460d129e88fbede
scottmotte <scott@scottmotte.com>
2013-08-26 15:46:27 -0400

    2 spacing tabs please vs the sometimes chosen 4

--- src/main/java/com/github/scottmotte/sendgrid/Sendgrid.java
+++ src/main/java/com/github/scottmotte/sendgrid/Sendgrid.java
@@ -5,105 +5,106 @@ import com.github.kevinsawicki.http.HttpRequest;
 import java.io.File;
 
 public class Sendgrid {
-    private static final String PARAM_API_KEY = "api_key";
-    private static final String PARAM_API_USER = "api_user";
-    private static final String PARAM_BCC = "bcc";
-    private static final String PARAM_FILES = "files[%s]";
-    private static final String PARAM_FROM = "from";
-    private static final String PARAM_HTML = "html";
-    private static final String PARAM_SUBJECT = "subject";
-    private static final String PARAM_TEXT = "text";
-    private static final String PARAM_TO = "to";
+  private static final String PARAM_API_KEY = "api_key";
+  private static final String PARAM_API_USER = "api_user";
+  private static final String PARAM_BCC = "bcc";
+  private static final String PARAM_FILES = "files[%s]";
+  private static final String PARAM_FROM = "from";
+  private static final String PARAM_HTML = "html";
+  private static final String PARAM_SUBJECT = "subject";
+  private static final String PARAM_TEXT = "text";
+  private static final String PARAM_TO = "to";
 
-    private String[] bcc;
-    private File[] file;
-    private String from;
-    private String html;
-    private String password;
-    private String subject;
-    private String text;
-    private String[] to;
-    private String username;
+  private String[] bcc;
+  private File[] file;
+  private String from;
+  private String html;
+  private String password;
+  private String subject;
+  private String text;
+  private String[] to;
+  private String username;
 
-    public static Sendgrid withCredentials(String username, String password) {
-        return new Sendgrid(username, password);
+  public static Sendgrid withCredentials(String username, String password) {
+    return new Sendgrid(username, password);
+  }
+
+  public Sendgrid(final String username, final String password) {
+    this.username = username;
+    this.password = password;
+  }
+
+  public Sendgrid bcc(final String... bcc) {
+    this.bcc = bcc;
+    return this;
+  }
+
+  public Sendgrid from(final String email) {
+    this.from = email;
+    return this;
+  }
+
+  public String send() {
+    HttpRequest request = HttpRequest.post("https://sendgrid.com/api/mail.send.json");
+    if (username != null) {
+      request.part(PARAM_API_USER, username);
+    }
+    if (password != null) {
+      request.part(PARAM_API_KEY, password);
+    }
+    if (from != null) {
+      request.part(PARAM_FROM, from);
+    }
+    if (to != null) {
+      for (String s : to) {
+        request.part(PARAM_TO, s);
+      }
+    }
+    if (bcc != null) {
+      for (String s : bcc) {
+        request.part(PARAM_BCC, s);
+      }
+    }
+    if (subject != null) {
+      request.part(PARAM_SUBJECT, subject);
+    }
+    if (text != null) {
+      request.part(PARAM_TEXT, text);
+    }
+    if (html != null) {
+      request.part(PARAM_HTML, html);
+    }
+    if (file != null) {
+      for (File f : file) {
+        request.part(String.format(PARAM_FILES, f.getName()), f);
+      }
     }
 
-    public Sendgrid(final String username, final String password) {
-        this.username = username;
-        this.password = password;
-    }
+    return request.body();
+  }
 
-    public Sendgrid bcc(final String... bcc) {
-        this.bcc = bcc;
-        return this;
-    }
+  public Sendgrid to(final String... to) {
+    this.to = to;
+    return this;
+  }
 
-    public Sendgrid from(final String email) {
-        this.from = email;
-        return this;
-    }
+  public Sendgrid withAttachment(final File... file) {
+    this.file = file;
+    return this;
+  }
 
-    public String send() {
-        HttpRequest request = HttpRequest.post("https://sendgrid.com/api/mail.send.json");
-        if (username != null) {
-            request.part(PARAM_API_USER, username);
-        }
-        if (password != null) {
-            request.part(PARAM_API_KEY, password);
-        }
-        if (from != null) {
-            request.part(PARAM_FROM, from);
-        }
-        if (to != null) {
-            for (String s : to) {
-                request.part(PARAM_TO, s);
-            }
-        }
-        if (bcc != null) {
-            for (String s : bcc) {
-                request.part(PARAM_BCC, s);
-            }
-        }
-        if (subject != null) {
-            request.part(PARAM_SUBJECT, subject);
-        }
-        if (text != null) {
-            request.part(PARAM_TEXT, text);
-        }
-        if (html != null) {
-            request.part(PARAM_HTML, html);
-        }
-        if (file != null) {
-            for (File f : file) {
-                request.part(String.format(PARAM_FILES, f.getName()), f);
-            }
-        }
-        return request.body();
-    }
+  public Sendgrid withHtml(final String html) {
+    this.html = html;
+    return this;
+  }
 
-    public Sendgrid to(final String... to) {
-        this.to = to;
-        return this;
-    }
+  public Sendgrid withSubject(final String subject) {
+    this.subject = subject;
+    return this;
+  }
 
-    public Sendgrid withAttachment(final File... file) {
-        this.file = file;
-        return this;
-    }
-
-    public Sendgrid withHtml(final String html) {
-        this.html = html;
-        return this;
-    }
-
-    public Sendgrid withSubject(final String subject) {
-        this.subject = subject;
-        return this;
-    }
-
-    public Sendgrid withText(final String text) {
-        this.text = text;
-        return this;
-    }
+  public Sendgrid withText(final String text) {
+    this.text = text;
+    return this;
+  }
 }
--- src/test/java/com/github/scottmotte/sendgrid/SendgridTest.java
+++ src/test/java/com/github/scottmotte/sendgrid/SendgridTest.java
@@ -7,71 +7,71 @@ import java.io.File;
 import static org.junit.Assert.assertEquals;
 
 public class SendgridTest {
-    private static final String FROM_EMAIL = "from@email.to";
-    private static final String PASSWORD = "password";
-    private static final String TO_ANOTHER_EMAIL = "bcc@email.to";
-    private static final String TO_EMAIL = "to@email.to";
-    private static final String USERNAME = "username";
+  private static final String FROM_EMAIL = "from@email.to";
+  private static final String TO_ANOTHER_EMAIL = "bcc@email.to";
+  private static final String TO_EMAIL = "to@email.to";
+  private static final String USERNAME = "username";
+  private static final String PASSWORD = "password";
 
-    @Test
-    public void testSendSuccess() {
-        String result = Sendgrid
-                .withCredentials(USERNAME, PASSWORD)
-                .from(FROM_EMAIL)
-                .to(TO_EMAIL)
-                .withSubject("This is a test subject")
-                .withText("This is a test text.")
-                .send();
-        assertEquals("{\"message\":\"success\"}", result);
-    }
+  @Test
+  public void testSendSuccess() {
+    String result = Sendgrid
+            .withCredentials(USERNAME, PASSWORD)
+            .from(FROM_EMAIL)
+            .to(TO_EMAIL)
+            .withSubject("This is a test subject")
+            .withText("This is a test text.")
+            .send();
+    assertEquals("{\"message\":\"success\"}", result);
+  }
 
-    @Test
-    public void testSendSuccessBcc() {
-        String result = Sendgrid
-                .withCredentials(USERNAME, PASSWORD)
-                .from(FROM_EMAIL)
-                .to(TO_EMAIL)
-                .bcc(TO_ANOTHER_EMAIL)
-                .withSubject("This is a test subject")
-                .withText("This is a test text.")
-                .send();
-        assertEquals("{\"message\":\"success\"}", result);
-    }
+  @Test
+  public void testSendSuccessBcc() {
+    String result = Sendgrid
+            .withCredentials(USERNAME, PASSWORD)
+            .from(FROM_EMAIL)
+            .to(TO_EMAIL)
+            .bcc(TO_ANOTHER_EMAIL)
+            .withSubject("This is a test subject")
+            .withText("This is a test text.")
+            .send();
+    assertEquals("{\"message\":\"success\"}", result);
+  }
 
-    @Test
-    public void testSendSuccessWithAttachment() {
-        File attachment = new File(getClass().getResource("/test.txt").getFile());
-        String result = Sendgrid
-                .withCredentials(USERNAME, PASSWORD)
-                .from(FROM_EMAIL)
-                .to(TO_EMAIL)
-                .withSubject("This is a test subject")
-                .withText("This is a test text.")
-                .withAttachment(attachment)
-                .send();
-        assertEquals("{\"message\":\"success\"}", result);
-    }
+  @Test
+  public void testSendSuccessWithAttachment() {
+    File attachment = new File(getClass().getResource("/test.txt").getFile());
+    String result = Sendgrid
+            .withCredentials(USERNAME, PASSWORD)
+            .from(FROM_EMAIL)
+            .to(TO_EMAIL)
+            .withSubject("This is a test subject")
+            .withText("This is a test text.")
+            .withAttachment(attachment)
+            .send();
+    assertEquals("{\"message\":\"success\"}", result);
+  }
 
-    @Test
-    public void testSendSuccessWithMultipleRecipients() {
-        String result = Sendgrid
-                .withCredentials(USERNAME, PASSWORD)
-                .from(FROM_EMAIL)
-                .to(TO_EMAIL, TO_ANOTHER_EMAIL)
-                .withSubject("This is a test subject")
-                .withText("This is a test text.")
-                .send();
-        assertEquals("{\"message\":\"success\"}", result);
-    }
+  @Test
+  public void testSendSuccessWithMultipleRecipients() {
+    String result = Sendgrid
+            .withCredentials(USERNAME, PASSWORD)
+            .from(FROM_EMAIL)
+            .to(TO_EMAIL, TO_ANOTHER_EMAIL)
+            .withSubject("This is a test subject")
+            .withText("This is a test text.")
+            .send();
+    assertEquals("{\"message\":\"success\"}", result);
+  }
 
-    @Test
-    public void testSendWithoutFrom() {
-        String result = Sendgrid
-                .withCredentials(USERNAME, PASSWORD)
-                .to(TO_EMAIL)
-                .withSubject("This is a test subject")
-                .withText("This is a test text.")
-                .send();
-        assertEquals("{\"message\": \"error\", \"errors\": [\"Empty from email address (required)\"]}", result);
-    }
+  @Test
+  public void testSendWithoutFrom() {
+    String result = Sendgrid
+            .withCredentials(USERNAME, PASSWORD)
+            .to(TO_EMAIL)
+            .withSubject("This is a test subject")
+            .withText("This is a test text.")
+            .send();
+    assertEquals("{\"message\": \"error\", \"errors\": [\"Empty from email address (required)\"]}", result);
+  }
 }
