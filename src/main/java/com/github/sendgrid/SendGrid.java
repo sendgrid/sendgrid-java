b1e909cdbc49c81ca54656a09bcf5f815637b142
elbuo8 <yamil.asusta@sendgrid.com>
2014-04-04 01:01:48 -0400

    massive refactor with unittest still missing real test

--- src/main/java/com/github/sendgrid/SendGrid.java
+++ src/main/java/com/github/sendgrid/SendGrid.java
@@ -1,228 +1,45 @@
 package com.github.sendgrid;
 
-import com.github.kevinsawicki.http.HttpRequest;
 import org.json.JSONObject;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
+import com.mashape.unirest.http.*;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class SendGrid {
-  private static final String PARAM_API_USER    = "api_user";
-  private static final String PARAM_API_KEY     = "api_key";
-  private static final String PARAM_TOS         = "to[]";
-  private static final String PARAM_TONAMES     = "toname[]";
-  private static final String PARAM_BCC         = "bcc";
-  private static final String PARAM_FROM        = "from";
-  private static final String PARAM_FROMNAME    = "fromname";
-  private static final String PARAM_REPLYTO     = "replyto";
-  private static final String PARAM_SUBJECT     = "subject";
-  private static final String PARAM_HTML        = "html";
-  private static final String PARAM_TEXT        = "text";
-  private static final String PARAM_FILES       = "files[%s]";
-  private static final String PARAM_HEADERS     = "headers";
 
   private String username;
   private String password;
-  private ArrayList<String> tos = new ArrayList<String>();
-  private ArrayList<String> tonames = new ArrayList<String>();
-  private String bcc;
-  private String from;
-  private String fromname;
-  private String replyto;
-  private String subject;
-  private String text;
-  private String html;
-  private ArrayList<Attachment> attachments = new ArrayList<Attachment>();
-  private JSONObject headers = new JSONObject();
-  
+  private String url;
+  private int port;
+  private String endpoint;
+
   public SendGrid(String username, String password) {
     this.username = username;
     this.password = password;
+    this.url = "https://api.sendgrid.com";
+    this.port = 447;
+    this.endpoint = "/api/mail.send.json";
   }
 
-  public String send() {
-    return this.web();
-  } 
-
-  public String web() {
-    HttpRequest request = HttpRequest.post("https://api.sendgrid.com/api/mail.send.json");
-
-    if (this.username != null) {
-      request.part(PARAM_API_USER,  this.username);
-    }
-    if (this.password != null) {
-      request.part(PARAM_API_KEY,   this.password);
-    }
-    for (String to:this.getTos()) {
-      request.part(PARAM_TOS,     to);
-    }
-    for (String toname:this.getToNames()) {
-      request.part(PARAM_TONAMES,     toname);
-    }
-    if (this.getFrom() != null) {
-      request.part(PARAM_FROM,      this.getFrom());
-    }
-    if (this.getFromName() != null) {
-      request.part(PARAM_FROMNAME,  this.getFromName());
-    }
-    if (this.getReplyTo() != null) {
-      request.part(PARAM_REPLYTO, this.getReplyTo());
-    }
-    if (this.getSubject() != null) {
-      request.part(PARAM_SUBJECT,   this.getSubject());
-    }
-    if (this.getText() != null) {
-      request.part(PARAM_TEXT,    this.getText());
-    }
-    if (this.getHtml() != null) {
-      request.part(PARAM_HTML,    this.getHtml());
-    }
-    if (this.getBcc() != null) {
-      request.part(PARAM_BCC, this.getBcc());
-    }
-    for (Attachment attachment:this.getAttachments()) {
-      request.part(String.format(PARAM_FILES, attachment.name), attachment.contents);
-    }
-    request.part(PARAM_HEADERS,   this.getHeaders().toString());
-
-    return request.body();
-  }
-
-  public ArrayList<String> getTos() {
-    return this.tos;
-  }
-
-  public ArrayList<String> getToNames() {
-    return this.tonames;
-  }
-
-  public String getBcc() {
-    return this.bcc;
-  }
-
-  public String getFrom() {
-    return this.from;
-  }
-
-  public String getFromName() {
-    return this.fromname;
-  }
-
-  public String getReplyTo() {
-    return this.replyto;
-  }
-
-  public String getSubject() {
-    return this.subject;
-  }
-
-  public String getText() {
-    return this.text;
-  }
-
-  public String getHtml() {
-    return this.html;
-  }
-
-  public ArrayList<Attachment> getAttachments() {
-    return this.attachments;
-  }
-
-  public JSONObject getHeaders() {
-    return this.headers;
-  }
-
-  public SendGrid addTo(String email) {
-    this.tos.add(email);
+  public SendGrid setUrl(String url) {
+    this.url = url;
     return this;
   }
 
-  public SendGrid addToName(String name) {
-    this.tonames.add(name);
+  public SendGrid setPort(int port) {
+    this.port = port;
     return this;
   }
 
-  public SendGrid setFrom(String email) {
-    this.from = email;
+  public SendGrid setEndpoint(String endpoint) {
+    this.endpoint = endpoint;
     return this;
   }
 
-  public SendGrid setFromName(String name) {
-    this.fromname = name;
-    return this;
+  public void send(Mail mail) throws UnirestException {
+    HttpResponse<JsonNode> response = Unirest.post(this.url + ":" + this.port + this.endpoint)
+    .fields(mail.buildBody()).field("api_user", this.username).field("api_key", this.password).asJson();
   }
 
-  public SendGrid setReplyTo(String email) {
-    this.replyto = email;
-    return this;
-  }
-
-  public SendGrid setBcc(String bcc) {
-    this.bcc = bcc;
-    return this;
-  }
-
-  public SendGrid setSubject(String subject) {
-    this.subject = subject;
-    return this;
-  }
-
-  public SendGrid setText(String text) {
-    this.text = text;
-    return this;
-  }
-
-  public SendGrid setHtml(String html) {
-    this.html = html;
-    return this;
-  }
-
-  public SendGrid addFile(Attachment attachment) {
-    this.addAttachment(attachment);
-    return this;
-  }
-
-  public SendGrid addFile(File file) throws FileNotFoundException {
-    this.addAttachment(file);
-    return this;
-  }
-
-  public SendGrid addAttachment(Attachment attachment) {
-    this.attachments.add(attachment);
-    return this;
-  }
-
-  public SendGrid addAttachment(File file) throws FileNotFoundException {
-    SendGrid.Attachment attachment = new SendGrid.Attachment(file);
-    this.addAttachment(attachment);
-    return this;
-  }
-
-  public SendGrid addHeader(String key, String value) {
-    try {
-      this.headers.put(key, value);
-    } catch(Exception e){
-      e.printStackTrace();
-    }
-
-    return this;
-  }
-
-  public static class Attachment {
-    public final String name;
-    public final InputStream contents;
-
-    public Attachment(File file) throws FileNotFoundException {
-      this.name = file.getName();
-      this.contents = new FileInputStream(file);
-    }
-
-    public Attachment(String name, InputStream contents) {
-      this.name = name;
-      this.contents = contents;
-    }
-  }
 }
--- src/test/java/com/github/sendgrid/SendGridTest.java
+++ src/test/java/com/github/sendgrid/SendGridTest.java
@@ -2,179 +2,101 @@ package com.github.sendgrid;
 
 import org.json.JSONException;
 import org.junit.Test;
+import org.junit.Before;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.io.IOException; 
+import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static org.junit.matchers.JUnitMatchers.hasItems;
 
 public class SendGridTest {
+
+  Mail mail;
+
   private static final String USERNAME = "username";
   private static final String PASSWORD = "password";
-  
-  @Test public void sanityTestSend() {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
 
-    String email = "email@example.com";
-    sendgrid.addTo(email);
-    sendgrid.addToName("Name Guy");
-    sendgrid.setFrom(email);
-    sendgrid.setFromName("Some Name");
-    sendgrid.setReplyTo("no-reply@nowhere.com");
-    sendgrid.setSubject("Subject");
-    sendgrid.setText("Text");
-    sendgrid.setHtml("Html");
-    sendgrid.addHeader("key", "value");
-    String response = sendgrid.send();
-
-    assertEquals(response, "{\"message\": \"error\", \"errors\": [\"Bad username / password\"]}");
+  @Before public void initialize() {
+    mail = new Mail();
   }
 
-  @Test
-  public void testAddTo() {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
+  @Test public void testAddTo() {
     String email = "email@example.com";
-    sendgrid.addTo(email);
-
-    assertThat(sendgrid.getTos(), hasItems(email)); 
+    mail.addTo(email);
+    assertThat(mail.getTo(), hasItems(email));
   }
 
-  @Test
-  public void testAddToName() {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
+  @Test public void testAddToName() {
     String name = "Example Guy";
-    sendgrid.addToName(name);
-
-    assertThat(sendgrid.getToNames(), hasItems(name)); 
+    mail.addToName(name);
+    assertThat(mail.getToName(), hasItems(name));
   }
 
-  @Test
-  public void testSetBcc() {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
+  @Test public void testAddBcc() {
     String email = "email@example.com";
-    sendgrid.setBcc(email);
-
-    assertEquals(sendgrid.getBcc(), email); 
+    mail.addBcc(email);
+    assertThat(mail.getBcc(), hasItems(email));
   }
- 
-  @Test
-  public void testSetFrom() {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
 
+  @Test public void testSetFrom() {
     String email = "email@example.com";
-    sendgrid.setFrom(email);
-
-    assertEquals(sendgrid.getFrom(), email); 
+    mail.setFrom(email);
+    assertEquals(mail.getFrom(), email);
   }
 
-  @Test
-  public void testSetFromName() {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
+  @Test public void testSetFromName() {
     String name = "Example Guy";
-    sendgrid.setFromName(name);
-
-    assertEquals(sendgrid.getFromName(), name); 
+    mail.setFromName(name);
+    assertEquals(mail.getFromName(), name);
   }
 
-  @Test
-  public void testSetReplyTo() {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
+  @Test public void testSetReplyTo() {
     String email = "email@example.com";
-    sendgrid.setReplyTo(email);
-
-    assertEquals(sendgrid.getReplyTo(), email); 
+    mail.setReplyTo(email);
+    assertEquals(mail.getReplyTo(), email);
   }
 
-  @Test
-  public void testSetSubject() {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
+  @Test public void testSetSubject() {
     String subject = "This is a subject";
-    sendgrid.setSubject(subject);
-
-    assertEquals(sendgrid.getSubject(), subject); 
+    mail.setSubject(subject);
+    assertEquals(mail.getSubject(), subject);
   }
 
-  @Test
-  public void testSetText() {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
+  @Test public void testSetText() {
     String text = "This is some email text.";
-    sendgrid.setText(text);
-
-    assertEquals(sendgrid.getText(), text); 
+    mail.setText(text);
+    assertEquals(mail.getText(), text);
   }
 
-  @Test
-  public void testSetHtml() {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
+  @Test public void testSetHtml() {
     String html = "This is some email text.";
-    sendgrid.setHtml(html);
-
-    assertEquals(sendgrid.getHtml(), html); 
+    mail.setHtml(html);
+    assertEquals(mail.getHtml(), html);
   }
 
-  @Test
-  public void testAddFile() throws FileNotFoundException {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
+  @Test public void testAddAttachment() throws FileNotFoundException {
     File file = new File(getClass().getResource("/test.txt").getFile());
-    sendgrid.addFile(file);
-
-    assertEquals(sendgrid.getAttachments().get(0).name, file.getName());
+    mail.addAttachment(file, "test.txt");
+    assertTrue(mail.getAttachments().keySet().contains("test.txt"));
   }
 
-  @Test
-  public void testAddFileFromString() throws FileNotFoundException {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
-    try {
-      InputStream is = new FileInputStream(getClass().getResource("/test.txt").getFile());
-      is.close(); 
-
-      SendGrid.Attachment attachment1 = new SendGrid.Attachment("filename.txt", is);
-      sendgrid.addFile(attachment1);
-      assertThat(sendgrid.getAttachments(), hasItems(attachment1));
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  @Test public void testBuildBody() throws FileNotFoundException {
+    mail.addTo("yamil@sendgrid.com");
+    mail.addToName("Yamil");
+    mail.addBcc("jose@sendgrid.com");
+    mail.setFrom("yamil@sendgrid.com");
+    mail.setFromName("Yamil");
+    mail.setReplyTo("yamil@sendgrid.com");
+    mail.setSubject("Test");
+    mail.setText("Test body");
+    mail.setHtml("Test body");
+    File file = new File(getClass().getResource("/test.txt").getFile());
+    mail.addAttachment(file, "test.txt");
+    System.out.println(mail.buildBody().toString());
   }
 
-  @Test
-  public void testAddMultipleFiles() throws FileNotFoundException {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
-    File file1 = new File(getClass().getResource("/test.txt").getFile());
-    File file2 = new File(getClass().getResource("/image.png").getFile());
-
-    sendgrid.addAttachment(file1);
-    sendgrid.addFile(file2);
-
-    assertEquals(sendgrid.getAttachments().get(0).name, file1.getName());
-    assertEquals(sendgrid.getAttachments().get(1).name, file2.getName());
-  }
-
-  @Test
-  public void testAddHeader() throws JSONException {
-    SendGrid sendgrid = new SendGrid(USERNAME, PASSWORD);
-
-    sendgrid.addHeader("key", "value");
-    sendgrid.addHeader("other", "other-value");
-
-    assertEquals(sendgrid.getHeaders().get("key"), "value"); 
-    assertEquals(sendgrid.getHeaders().get("other"), "other-value"); 
-  }
 }
