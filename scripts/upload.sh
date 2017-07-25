e1727792b17665acc7f0991dfd04225ea684912c
Elmer Thomas <elmer@thinkingserious.com>
2017-07-25 16:32:17 -0700

    Use https

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -5,11 +5,11 @@ VERSION="4.0.1"
 curl \
   -F "sdk=@./repo/com/sendgrid/sendgrid-java-$VERSION.jar" \
   -H "X-Key: $UPLOAD_SECRET" \
-  http://dx.sendgrid.com/upload
+  https://dx.sendgrid.com/upload
 
 curl \
   -F "sdk=@./repo/com/sendgrid/sendgrid-java-latest.jar" \
   -H "X-Key: $UPLOAD_SECRET" \
-  http://dx.sendgrid.com/upload
+  https://dx.sendgrid.com/upload
 
 exit 0
