bd1196e7e706893ef493e95c43575468da14de31
Andy Trimble <andy.trimble@gmail.com>
2017-10-05 17:24:09 -0600

    Removing the if check around download.

--- scripts/prism.sh
+++ scripts/prism.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [[ -z "$(command -v prism)" ]]; then
-  curl https://raw.githubusercontent.com/stoplightio/prism/master/install.sh | sh
-fi
-
+curl https://raw.githubusercontent.com/stoplightio/prism/master/install.sh | sh
 prism run --mock --list --spec https://raw.githubusercontent.com/sendgrid/sendgrid-oai/master/oai_stoplight.json
 
