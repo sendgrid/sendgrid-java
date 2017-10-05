e64070bb4f8de693e9409c26673623a22b0eb089
Andy Trimble <andy.trimble@gmail.com>
2017-10-05 17:13:31 -0600

    Starting up a Prism server on test.

---
+++ scripts/prism.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ `command -v prism` == '' ]; then
+  curl https://raw.githubusercontent.com/stoplightio/prism/master/install.sh | sh
+fi
+
+prism run --mock --list --spec https://raw.githubusercontent.com/sendgrid/sendgrid-oai/master/oai_stoplight.json
+
