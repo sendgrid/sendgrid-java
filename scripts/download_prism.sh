213631c7ce81c64ff028a42cd4f80e0e4f8619ea
Andy Trimble <andy.trimble@gmail.com>
2017-10-06 09:09:07 -0600

    Fixed shutdown issue, updated gradle to 4.2.1

---
+++ scripts/download_prism.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+install () {
+
+set -eu
+
+UNAME=$(uname)
+ARCH=$(uname -m)
+if [ "$UNAME" != "Linux" ] && [ "$UNAME" != "Darwin" ] && [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "i686" ]; then
+    echo "Sorry, OS/Architecture not supported: ${UNAME}/${ARCH}. Download binary from https://github.com/stoplightio/prism/releases"
+    exit 1
+fi
+
+if [ "$UNAME" = "Darwin" ] ; then
+  OSX_ARCH=$(uname -m)
+  if [ "${OSX_ARCH}" = "x86_64" ] ; then
+    PLATFORM="darwin_amd64"
+  fi
+elif [ "$UNAME" = "Linux" ] ; then
+  LINUX_ARCH=$(uname -m)
+  if [ "${LINUX_ARCH}" = "i686" ] ; then
+    PLATFORM="linux_386"
+  elif [ "${LINUX_ARCH}" = "x86_64" ] ; then
+    PLATFORM="linux_amd64"
+  fi
+fi
+
+# LATEST=$(curl -s https://api.github.com/repos/stoplightio/prism/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
+URL="https://github.com/stoplightio/prism/releases/download/v0.6.21/prism_$PLATFORM"
+SRC=$(pwd)/prism_$PLATFORM
+DEST=./prism
+
+# if [ -z $LATEST ] ; then
+#   echo "Error requesting. Download binary from ${URL}"
+#   exit 1
+# else
+  STATUS=$(curl -sL -w %{http_code} -o $SRC $URL)
+  if [ $STATUS -ge 200 ] & [ $STATUS -le 308 ]; then
+    mv $SRC $DEST
+    chmod +x $DEST
+    echo "Prism installation was successful"
+  else
+    rm $SRC
+    echo "Error requesting. Download binary from ${URL}"
+    exit 1
+  fi
+# fi
+}
+
+if [ ! -f prism ]; then
+  install
+fi
--- scripts/prism.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl https://raw.githubusercontent.com/stoplightio/prism/master/install.sh | sh
-prism run --mock --list --spec https://raw.githubusercontent.com/sendgrid/sendgrid-oai/master/oai_stoplight.json
-
