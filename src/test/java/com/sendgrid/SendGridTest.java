ba4a345d7821c22663a37703652a1a681d936ea1
Sabih Ismail <sabihismail@gmail.com>
2017-10-17 15:50:01 -0400

    Added Dropbox link test.

--- src/test/java/com/sendgrid/SendGridTest.java
+++ src/test/java/com/sendgrid/SendGridTest.java
@@ -4692,4 +4692,22 @@ public class SendGridTest {
     Assert.assertEquals(200, response.getStatusCode());
   }
 
+  @Test
+  public void dropboxUpload() throws IOException {
+    Email from = new Email("example@example.com");
+    String subject = "Sending with SendGrid is Fun";
+    Email to = new Email("example@example.com");
+    Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+    Mail mail = new Mail(from, subject, to, content);
+    mail.addDropboxAttachment("www.dropbox.com/s/w5cbrfd0cn8iax0/Systems_Development_Life_Cycle.jpg?dl=0");
+
+    SendGrid sg = new SendGrid(SENDGRID_API_KEY);
+    Request request = new Request();
+    request.setMethod(Method.POST);
+    request.setEndpoint("mail/send");
+    request.setBody(mail.build());
+
+    Response response = sg.api(request);
+    Assert.assertEquals(202, response.getStatusCode());
+  }
 }
