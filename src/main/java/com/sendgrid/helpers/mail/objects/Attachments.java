45d1de20a1082a3b903328528d5d51991f194590
Luka Atanasovski <lukaatanasovski@hotmail.com>
2017-10-19 00:12:40 +0200

    add methods to encode raw file to base64

--- src/main/java/com/sendgrid/helpers/mail/objects/Attachments.java
+++ src/main/java/com/sendgrid/helpers/mail/objects/Attachments.java
@@ -184,6 +184,48 @@ public class Attachments {
       this.content = content;
     }
 
+
+  /**
+   * You can use this method to encode a raw file to b64
+   * @param  fileName    the name of the file to include
+   * @throws IOException if the file wasnt processed correctly
+   */
+  private String encodeFileToBase64Binary() throws RuntimeException {
+    File file = new File(fileName);
+    try {
+      byte[] bytes = loadFile(file);
+    } catch (IOException e) {
+      throw new RuntimeException("Unable to convert content stream to base 64 encoded string", e);
+    }
+    byte[] encoded = Base64.encodeBase64(bytes);
+    String encodedString = new String(encoded);
+    return encodedString;
+  }
+  /**
+   * Read a raw file into a byte[].
+   * @param  file        the file to read
+   * @throws IOException if the file wasnt processed correctly
+   */
+  private static byte[] loadFile(File file) throws IOException {
+    InputStream is = new FileInputStream(file);
+    long length = file.length();
+    if (length > Integer.MAX_VALUE) {
+      // File is too large
+    }
+    byte[] bytes = new byte[(int)length];
+    int offset = 0;
+    int numRead = 0;
+    while (offset < bytes.length
+           && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+      offset += numRead;
+    }
+    if (offset < bytes.length) {
+      throw new IOException("Could not completely read file " + fileName);
+    }
+    is.close();
+    return bytes;
+  }
+
     private String encodeToBase64(InputStream content) {
       int read = 0;
       byte[] bytes = new byte[BYTE_BUFFER_SIZE];
