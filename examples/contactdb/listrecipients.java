711217924795279c26787302ea6d3d1313e03208
teisena <teisena@protonmail.ch>
2017-10-28 02:21:13 +0300

    split lists/recipients endpoint

--- examples/contactdb/listrecipients.java
+++ examples/contactdb/listrecipients.java
@@ -8,8 +8,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 //////////////////////////////////////////////////////////////////
-// Create a List
-// POST /contactdb/lists
+// Add Multiple Recipients to a List
+// POST /contactdb/lists/{list_id}/recipients
 
 
 public class Example {
@@ -18,8 +18,8 @@ public class Example {
       SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
       Request request = new Request();
       request.setMethod(Method.POST);
-      request.setEndpoint("contactdb/lists");
-      request.setBody("{\"name\":\"your list name\"}");
+      request.setEndpoint("contactdb/lists/{list_id}/recipients");
+      request.setBody("[\"recipient_id1\",\"recipient_id2\"]");
       Response response = sg.api(request);
       System.out.println(response.getStatusCode());
       System.out.println(response.getBody());
@@ -31,8 +31,8 @@ public class Example {
 }
 
 //////////////////////////////////////////////////////////////////
-// Retrieve all lists
-// GET /contactdb/lists
+// Retrieve all recipients on a List
+// GET /contactdb/lists/{list_id}/recipients
 
 
 public class Example {
@@ -41,53 +41,9 @@ public class Example {
       SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
       Request request = new Request();
       request.setMethod(Method.GET);
-      request.setEndpoint("contactdb/lists");
-      Response response = sg.api(request);
-      System.out.println(response.getStatusCode());
-      System.out.println(response.getBody());
-      System.out.println(response.getHeaders());
-    } catch (IOException ex) {
-      throw ex;
-    }
-  }
-}
-
-//////////////////////////////////////////////////////////////////
-// Delete Multiple lists
-// DELETE /contactdb/lists
-
-
-public class Example {
-  public static void main(String[] args) throws IOException {
-    try {
-      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-      Request request = new Request();
-      request.setMethod(Method.DELETE);
-      request.setEndpoint("contactdb/lists");
-      request.setBody("[1,2,3,4]");
-      Response response = sg.api(request);
-      System.out.println(response.getStatusCode());
-      System.out.println(response.getBody());
-      System.out.println(response.getHeaders());
-    } catch (IOException ex) {
-      throw ex;
-    }
-  }
-}
-
-//////////////////////////////////////////////////////////////////
-// Update a List
-// PATCH /contactdb/lists/{list_id}
-
-
-public class Example {
-  public static void main(String[] args) throws IOException {
-    try {
-      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-      Request request = new Request();
-      request.setMethod(Method.PATCH);
-      request.setEndpoint("contactdb/lists/{list_id}");
-      request.setBody("{\"name\":\"newlistname\"}");
+      request.setEndpoint("contactdb/lists/{list_id}/recipients");
+      request.addQueryParam("page", "1");
+      request.addQueryParam("page_size", "1");
       request.addQueryParam("list_id", "1");
       Response response = sg.api(request);
       System.out.println(response.getStatusCode());
@@ -100,8 +56,8 @@ public class Example {
 }
 
 //////////////////////////////////////////////////////////////////
-// Retrieve a single list
-// GET /contactdb/lists/{list_id}
+// Add a Single Recipient to a List
+// POST /contactdb/lists/{list_id}/recipients/{recipient_id}
 
 
 public class Example {
@@ -109,9 +65,8 @@ public class Example {
     try {
       SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
       Request request = new Request();
-      request.setMethod(Method.GET);
-      request.setEndpoint("contactdb/lists/{list_id}");
-      request.addQueryParam("list_id", "1");
+      request.setMethod(Method.POST);
+      request.setEndpoint("contactdb/lists/{list_id}/recipients/{recipient_id}");
       Response response = sg.api(request);
       System.out.println(response.getStatusCode());
       System.out.println(response.getBody());
@@ -123,8 +78,8 @@ public class Example {
 }
 
 //////////////////////////////////////////////////////////////////
-// Delete a List
-// DELETE /contactdb/lists/{list_id}
+// Delete a Single Recipient from a Single List
+// DELETE /contactdb/lists/{list_id}/recipients/{recipient_id}
 
 
 public class Example {
@@ -133,8 +88,9 @@ public class Example {
       SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
       Request request = new Request();
       request.setMethod(Method.DELETE);
-      request.setEndpoint("contactdb/lists/{list_id}");
-      request.addQueryParam("delete_contacts", "true");
+      request.setEndpoint("contactdb/lists/{list_id}/recipients/{recipient_id}");
+      request.addQueryParam("recipient_id", "1");
+      request.addQueryParam("list_id", "1");
       Response response = sg.api(request);
       System.out.println(response.getStatusCode());
       System.out.println(response.getBody());
