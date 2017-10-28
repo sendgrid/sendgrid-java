ff447cfd68844d30e0d274259ff18bb0a998c4f5
Vidya Sagar Bula <vidyasagar.bula@ad.infosys.com>
2017-10-28 12:13:02 +0530

    added examples of subusers into different classes

---
+++ src/main/java/subusers/examples/Example10.java
@@ -0,0 +1,27 @@
+package subusers.examples;
+
+import java.io.IOException;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+
+public class Example10 {
+
+	public static void executeTest() throws IOException{
+		try {
+		      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+		      Request request = new Request();
+		      request.setMethod(Method.PUT);
+		      request.setEndpoint("subusers/{subuser_name}/monitor");
+		      request.setBody("{\"frequency\":500,\"email\":\"example@example.com\"}");
+		      Response response = sg.api(request);
+		      System.out.println(response.getStatusCode());
+		      System.out.println(response.getBody());
+		      System.out.println(response.getHeaders());
+		    } catch (IOException ex) {
+		      throw ex;
+		    }
+	}
+}
---
+++ src/main/java/subusers/examples/Example12.java
@@ -0,0 +1,26 @@
+package subusers.examples;
+
+import java.io.IOException;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+
+public class Example12 {
+
+	public static void executeTest() throws IOException{
+		   try {
+			      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+			      Request request = new Request();
+			      request.setMethod(Method.GET);
+			      request.setEndpoint("subusers/{subuser_name}/monitor");
+			      Response response = sg.api(request);
+			      System.out.println(response.getStatusCode());
+			      System.out.println(response.getBody());
+			      System.out.println(response.getHeaders());
+			    } catch (IOException ex) {
+			      throw ex;
+			    }
+	}
+}
