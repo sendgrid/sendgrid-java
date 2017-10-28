7017f87da96f17477392167259d2ee5e444fe66d
saritha-infosys <33170971+saritha-infosys@users.noreply.github.com>
2017-10-28 14:11:27 +0530

    Create SimpleEmail.java
    Adding simple email using java sdk

---
+++ src/main/java/com/sendgrid/SimpleEmail.java
@@ -0,0 +1,73 @@
+package email;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SimpleSMTPEmail {
+	public static void main(String[] args) {
+	      // Recipient's email ID needs to be mentioned.
+	      String to = "to@gmail.com";
+
+	      // Sender's email ID needs to be mentioned
+	      String from = "from@gmail.com";
+	      final String username = "username";//change accordingly
+	      final String password = "pwd";//change accordingly
+
+	      // Assuming you are sending email through relay.jangosmtp.net
+	      String host = "smtp.gmail.com";
+
+	      Properties props = new Properties();
+	      props.put("mail.smtp.auth", "true");
+	      props.put("mail.smtp.starttls.enable", "true");
+	      props.put("mail.smtp.host", host);
+	      props.put("mail.smtp.port", "25");
+
+	      // Get the Session object.
+	      Session session = Session.getInstance(props,
+	         new javax.mail.Authenticator() {
+	            protected PasswordAuthentication getPasswordAuthentication() {
+	               return new PasswordAuthentication(username, password);
+		   }
+	         });
+
+	      try {
+		   // Create a default MimeMessage object.
+		   Message message = new MimeMessage(session);
+		
+		   // Set From: header field of the header.
+		   message.setFrom(new InternetAddress(from));
+		
+		   // Set To: header field of the header.
+		   message.setRecipients(Message.RecipientType.TO,
+	               InternetAddress.parse(to));
+		
+		   // Set Subject: header field
+		   message.setSubject("Testing Subject");
+		
+		   // Now set the actual message
+		   message.setText("Hello, this is sample for to check send " +
+			"email using JavaMailAPI ");
+
+		   // Send message
+		   Transport.send(message);
+
+		   System.out.println("Sent message successfully....");
+
+	      } catch (MessagingException e) {
+	         throw new RuntimeException(e);
+	      }
+	   }
+	}
+
+　
+　
+　
+　
+　
