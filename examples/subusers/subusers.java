39284ca87eef8bbaf3e4e10a3b075971f3a032a2
Somesh Mehta <mehta.somesh636@gmail.com>
2017-10-31 03:51:27 +0530

    Update subusers.java

--- examples/subusers/subusers.java
+++ examples/subusers/subusers.java
@@ -52,8 +52,29 @@ public class Example {
   }
 }
 
-
 //////////////////////////////////////////////////////////////////
+// Retrieve Subuser Reputations
+// GET /subusers/reputations
+
+
+public class Example {
+  public static void main(String[] args) throws IOException {
+    try {
+      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+      Request request = new Request();
+      request.setMethod(Method.GET);
+      request.setEndpoint("subusers/reputations");
+      request.addQueryParam("usernames", "test_string");
+      Response response = sg.api(request);
+      System.out.println(response.getStatusCode());
+      System.out.println(response.getBody());
+      System.out.println(response.getHeaders());
+    } catch (IOException ex) {
+      throw ex;
+    }
+  }
+}
+
 
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
