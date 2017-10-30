8e13f7570f3da23cbd2a82589780f071de4a4ef3
Somesh Mehta <mehta.somesh636@gmail.com>
2017-10-31 03:50:18 +0530

    Update emailstatistics.java

--- examples/subusers/emailstatistics.java
+++ examples/subusers/emailstatistics.java
@@ -24,3 +24,29 @@ public class Example {
     }
   }
 }
+//////////////////////////////////////////////////////////////////
+// Retrieve the monthly email statistics for a single subuser
+// GET /subusers/{subuser_name}/stats/monthly
+
+
+public class Example {
+  public static void main(String[] args) throws IOException {
+    try {
+      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+      Request request = new Request();
+      request.setMethod(Method.GET);
+      request.setEndpoint("subusers/{subuser_name}/stats/monthly");
+      request.addQueryParam("date", "test_string");
+      request.addQueryParam("sort_by_direction", "asc");
+      request.addQueryParam("limit", "1");
+      request.addQueryParam("sort_by_metric", "test_string");
+      request.addQueryParam("offset", "1");
+      Response response = sg.api(request);
+      System.out.println(response.getStatusCode());
+      System.out.println(response.getBody());
+      System.out.println(response.getHeaders());
+    } catch (IOException ex) {
+      throw ex;
+    }
+  }
+}
