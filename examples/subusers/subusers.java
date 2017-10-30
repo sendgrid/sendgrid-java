73370b905f18f70d9dfa9d81fb84ba45e00d4234
Somesh Mehta <mehta.somesh636@gmail.com>
2017-10-31 03:52:43 +0530

    Update subusers.java

--- examples/subusers/subusers.java
+++ examples/subusers/subusers.java
@@ -75,8 +75,30 @@ public class Example {
   }
 }
 
-
 //////////////////////////////////////////////////////////////////
+// Enable/disable a subuser
+// PATCH /subusers/{subuser_name}
+
+
+public class Example {
+  public static void main(String[] args) throws IOException {
+    try {
+      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+      Request request = new Request();
+      request.setMethod(Method.PATCH);
+      request.setEndpoint("subusers/{subuser_name}");
+      request.setBody("{\"disabled\":false}");
+      Response response = sg.api(request);
+      System.out.println(response.getStatusCode());
+      System.out.println(response.getBody());
+      System.out.println(response.getHeaders());
+    } catch (IOException ex) {
+      throw ex;
+    }
+  }
+}
+
+
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
