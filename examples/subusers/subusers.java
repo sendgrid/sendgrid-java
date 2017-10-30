09608ca230d5b2eae074823bd647adf8b4a32dac
Somesh Mehta <mehta.somesh636@gmail.com>
2017-10-31 03:45:21 +0530

    Update subusers.java

--- examples/subusers/subusers.java
+++ examples/subusers/subusers.java
@@ -35,27 +35,6 @@ public class Example {
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
-// Delete a subuser
-// DELETE /subusers/{subuser_name}
-
-
-public class Example {
-  public static void main(String[] args) throws IOException {
-    try {
-      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-      Request request = new Request();
-      request.setMethod(Method.DELETE);
-      request.setEndpoint("subusers/{subuser_name}");
-      Response response = sg.api(request);
-      System.out.println(response.getStatusCode());
-      System.out.println(response.getBody());
-      System.out.println(response.getHeaders());
-    } catch (IOException ex) {
-      throw ex;
-    }
-  }
-}
-
 //////////////////////////////////////////////////////////////////
 // Update IPs assigned to a subuser
 // PUT /subusers/{subuser_name}/ips
