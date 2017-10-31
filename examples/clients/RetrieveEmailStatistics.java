07a690a50f62d6554dc37c879950fefe72e95db9
knighthedspi <knighthedspi@gmail.com>
2017-10-31 15:13:01 +0900

    Fix "similar-code" issue in examples/clients/clients.java

---
+++ examples/clients/RetrieveEmailStatistics.java
@@ -0,0 +1,33 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.sendgrid.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+//////////////////////////////////////////////////////////////////
+// Retrieve email statistics by client type.
+// GET /clients/stats
+
+
+public class RetrieveEmailStatistics {
+  public static void main(String[] args) throws IOException {
+    try {
+      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+      Request request = new Request();
+      request.setMethod(Method.GET);
+      request.setEndpoint("clients/stats");
+      request.addQueryParam("aggregated_by", "day");
+      request.addQueryParam("start_date", "2016-01-01");
+      request.addQueryParam("end_date", "2016-04-01");
+      Response response = sg.api(request);
+      System.out.println(response.getStatusCode());
+      System.out.println(response.getBody());
+      System.out.println(response.getHeaders());
+    } catch (IOException ex) {
+      throw ex;
+    }
+  }
+}
--- examples/clients/RetrieveStats.java
+++ examples/clients/RetrieveStats.java
@@ -7,37 +7,13 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-//////////////////////////////////////////////////////////////////
-// Retrieve email statistics by client type.
-// GET /clients/stats
-
-
-public class Example {
-  public static void main(String[] args) throws IOException {
-    try {
-      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-      Request request = new Request();
-      request.setMethod(Method.GET);
-      request.setEndpoint("clients/stats");
-      request.addQueryParam("aggregated_by", "day");
-      request.addQueryParam("start_date", "2016-01-01");
-      request.addQueryParam("end_date", "2016-04-01");
-      Response response = sg.api(request);
-      System.out.println(response.getStatusCode());
-      System.out.println(response.getBody());
-      System.out.println(response.getHeaders());
-    } catch (IOException ex) {
-      throw ex;
-    }
-  }
-}
 
 //////////////////////////////////////////////////////////////////
 // Retrieve stats by a specific client type.
 // GET /clients/{client_type}/stats
 
 
-public class Example {
+public class RetrieveStats {
   public static void main(String[] args) throws IOException {
     try {
       SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
