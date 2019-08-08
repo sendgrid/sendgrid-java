702c62a84dbf5b8d28d7e52dedff82e74c77deba
Alberto Rivera <albertor24.r@gmail.com>
2019-08-07 21:47:15 -0700

    [582] - Docker container will now be testable

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,15 +13,9 @@ SENDGRID_JAVA_VERSION="1.0.0"
 echo "Welcome to sendgrid-java docker v${SENDGRID_JAVA_VERSION}."
 echo
 
-if [ "$1" != "--no-mock" ]
-then
-	echo "Starting Prism in mock mode. Calls made to Prism will not actually send emails."
-	echo "Disable this by running this container with --no-mock."
-	prism run --mock --spec $OAI_SPEC_URL 2> /dev/null &
-else
-	echo "Starting Prism in live (--no-mock) mode. Calls made to Prism will send emails."
-	prism run --spec $OAI_SPEC_URL 2> /dev/null  &
-fi
+echo "Starting Prism in mock mode. Calls made to Prism will not actually send emails. Logs will be found at /tmp/prism.log"
+/root/sendgrid-java/gradlew -p /root/sendgrid-java startPrism >> /tmp/prism.log 2> /dev/null &
+
 echo "To use Prism, make API calls to localhost:4010. For example,"
 echo "    sg = sendgrid.SendGridAPIClient("
 echo "        host='http://localhost:4010/',"
