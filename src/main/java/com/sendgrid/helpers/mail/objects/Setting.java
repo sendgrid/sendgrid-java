31f6d6326c90ef2e295a4e34bbfec6d48d88eff0
Sam Harrison <sharrison@twilio.com>
2020-03-04 10:14:58 -0600

    fix: correct the serialization of Setting to include non-null values

--- src/main/java/com/sendgrid/helpers/mail/objects/Setting.java
+++ src/main/java/com/sendgrid/helpers/mail/objects/Setting.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonInclude(Include.NON_DEFAULT)
+@JsonInclude(Include.NON_NULL)
 public class Setting {
   @JsonProperty("enable") private boolean enable;
 
