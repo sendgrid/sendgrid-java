ae4c41f42ce771a3ce7944c9812155a7e4075f6b
Anuj Shah <anuj.shah@sendgrid.com>
2020-05-07 14:25:45 -0700

    signature-verification: consistent sample data across SDKs

--- examples/helpers/eventwebhook/Example.java
+++ examples/helpers/eventwebhook/Example.java
@@ -12,10 +12,10 @@ public class Example {
         Security.addProvider(new BouncyCastleProvider());
 
         try {
-            String publicKey = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEqTxd43gyp8IOEto2LdIfjRQrIbsd4SXZkLW6jDutdhXSJCWHw8REntlo7aNDthvj+y7GjUuFDb/R1NGe1OPzpA==";
-            String payload = "[{\"email\":\"example@test.com\",\"timestamp\":1583532164,\"smtp-id\":\"<14c5d75ce93.dfd.64b469@ismtpd-555>\",\"event\":\"processed\",\"category\":\"cat facts\",\"sg_event_id\":\"sg_event_id\",\"sg_message_id\":\"sg_message_id\"}]";
-            String signature = "MEUCIB3PweCEFCNC0kCi6XFmlDSeFV9INyue8XmKejxu6OrKAiEAguMqCpe/5HpuK21G/+QxcZSKQm3lzM4YAblVHAhoXeU=";
-            String timestamp = "1587162961";
+            String publicKey = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEEDr2LjtURuePQzplybdC+u4CwrqDqBaWjcMMsTbhdbcwHBcepxo7yAQGhHPTnlvFYPAZFceEu/1FwCM/QmGUhA==";
+            String payload = "{\"category\":\"example_payload\",\"event\":\"test_event\",\"message_id\":\"message_id\"}";
+            String signature = "MEUCIQCtIHJeH93Y+qpYeWrySphQgpNGNr/U+UyUlBkU6n7RAwIgJTz2C+8a8xonZGi6BpSzoQsbVRamr2nlxFDWYNH2j/0=";
+            String timestamp = "1588788367";
             EventWebhook ew = new EventWebhook();
             ECPublicKey ellipticCurvePublicKey = ew.ConvertPublicKeyToECDSA(publicKey);
             boolean valid = ew.VerifySignature(ellipticCurvePublicKey, payload, signature, timestamp);
--- src/test/java/com/sendgrid/helpers/eventwebhook/EventWebhookTest.java
+++ src/test/java/com/sendgrid/helpers/eventwebhook/EventWebhookTest.java
@@ -12,10 +12,10 @@ public class EventWebhookTest {
 
         Security.addProvider(new BouncyCastleProvider());
 
-        String testPublicKey = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE0wuSSQf427N670xAsZ5vKZcyA0d9+/YyGnLDN4mv11ikucYzBOSyPQlfKkWSD1mS7zYzljgt9iWw7jUFVrwFjg==";
-        String testPayload = "[{\"email\":\"example@test.com\",\"timestamp\":1581532164,\"smtp-id\":\"<14c5d75ce93.dfd.64b469@ismtpd-555>\",\"event\":\"processed\",\"category\":\"cat facts\",\"sg_event_id\":\"sg_event_id\",\"sg_message_id\":\"sg_message_id\"},\n{\"email\":\"example@test.com\",\"timestamp\":1581532164,\"smtp-id\":\"<14c5d75ce93.dfd.64b469@ismtpd-555>\",\"event\":\"deferred\",\"category\":\"cat facts\",\"sg_event_id\":\"sg_event_id\",\"sg_message_id\":\"sg_message_id\",\"response\":\"400 try again later\",\"attempt\":\"5\"},\n{\"email\":\"example@test.com\",\"timestamp\":1581532164,\"smtp-id\":\"<14c5d75ce93.dfd.64b469@ismtpd-555>\",\"event\":\"delivered\",\"category\":\"cat facts\",\"sg_event_id\":\"sg_event_id\",\"sg_message_id\":\"sg_message_id\",\"response\":\"250 OK\"}]\n";
-        String testSignature = "MEUCIQDrZLnEUsPfG6x2GLo7W6V8CJVqnQKs6PEfHKF4k8/rhQIgNmInuCti7RtcJMASqxHKnhNzPAmdIYCwJNTEk7E/nlY=";
-        String testTimestamp = "1587162873";
+        String testPublicKey = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEEDr2LjtURuePQzplybdC+u4CwrqDqBaWjcMMsTbhdbcwHBcepxo7yAQGhHPTnlvFYPAZFceEu/1FwCM/QmGUhA==";
+        String testPayload = "{\"category\":\"example_payload\",\"event\":\"test_event\",\"message_id\":\"message_id\"}";
+        String testSignature = "MEUCIQCtIHJeH93Y+qpYeWrySphQgpNGNr/U+UyUlBkU6n7RAwIgJTz2C+8a8xonZGi6BpSzoQsbVRamr2nlxFDWYNH2j/0=";
+        String testTimestamp = "1588788367";
 
         EventWebhook ew = new EventWebhook();
         ECPublicKey ellipticCurvePublicKey = ew.ConvertPublicKeyToECDSA(testPublicKey);
