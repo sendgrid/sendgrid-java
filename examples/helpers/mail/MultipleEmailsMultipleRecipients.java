2dfedef2a324665a7858ae6341af07219689a857
Sam Harrison <sharrison@twilio.com>
2020-07-16 14:29:26 -0500

    add dynamic template data to example

--- examples/helpers/mail/MultipleEmailsMultipleRecipients.java
+++ examples/helpers/mail/MultipleEmailsMultipleRecipients.java
@@ -16,13 +16,18 @@ public class MultipleEmailsMultipleRecipients {
 
     mail.setFrom(new Email("test@example.com", "Example User"));
     mail.setSubject("Sending with Twilio SendGrid is Fun");
+    mail.setTemplateId("d-12345678901234567890123456789012");
 
     final Personalization personalization1 = new Personalization();
     personalization1.addTo(new Email("test1@example.com", "Example User1"));
+    personalization1.addDynamicTemplateData("name", "Example User1");
+    personalization1.addDynamicTemplateData("city", "Denver");
     mail.addPersonalization(personalization1);
 
     final Personalization personalization2 = new Personalization();
     personalization2.addTo(new Email("test2@example.com", "Example User2"));
+    personalization2.addDynamicTemplateData("name", "Example User2");
+    personalization2.addDynamicTemplateData("city", "San Francisco");
     mail.addPersonalization(personalization2);
 
     mail.addContent(new Content("text/plain", "and easy to do anywhere, even with Java"));
