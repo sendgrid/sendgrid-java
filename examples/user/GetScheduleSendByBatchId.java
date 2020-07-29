195a658b5a99310b00ddd53b00a82cbf140ae97a
Adams Au <adamsau@hotmail.com.hk>
2020-07-30 03:14:38 +0800

    docs: breakup examples to their own files in examples/user/user.java (#370)

---
+++ examples/user/GetScheduleSendByBatchId.java
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////////
+// Retrieve scheduled send
+// GET /user/scheduled_sends/{batch_id}
+
+
+public class Example {
+  public static void main(String[] args) throws IOException {
+    try {
+      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+      Request request = new Request();
+      request.setMethod(Method.GET);
+      request.setEndpoint("user/scheduled_sends/{batch_id}");
+      Response response = sg.api(request);
+      System.out.println(response.getStatusCode());
+      System.out.println(response.getBody());
+      System.out.println(response.getHeaders());
+    } catch (IOException ex) {
+      throw ex;
+    }
+  }
+}
