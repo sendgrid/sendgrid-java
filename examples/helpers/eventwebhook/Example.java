c94a501a47515b466386ac06bac16d85bab1d79f
Sam Harrison <sharrison@twilio.com>
2020-09-15 14:26:32 -0500

    fix: update the eventwebhook sample data, example, tests, and byte handling

--- examples/helpers/eventwebhook/Example.java
+++ examples/helpers/eventwebhook/Example.java
@@ -1,27 +1,49 @@
 import com.sendgrid.helpers.eventwebhook.EventWebhook;
-import java.security.PublicKey;
+import com.sendgrid.helpers.eventwebhook.EventWebhookHeader;
+import com.twilio.security.RequestValidator;
+import com.twilio.twiml.MessagingResponse;
+import com.twilio.twiml.messaging.Body;
+import com.twilio.twiml.messaging.Message;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import spark.Route;
+
 import java.security.Security;
 import java.security.interfaces.ECPublicKey;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.post;
 
 public class Example {
-
     public static void main(String[] args) {
         Security.addProvider(new BouncyCastleProvider());
 
-        try {
-            String publicKey = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEEDr2LjtURuePQzplybdC+u4CwrqDqBaWjcMMsTbhdbcwHBcepxo7yAQGhHPTnlvFYPAZFceEu/1FwCM/QmGUhA==";
-            String payload = "{\"category\":\"example_payload\",\"event\":\"test_event\",\"message_id\":\"message_id\"}";
-            String signature = "MEUCIQCtIHJeH93Y+qpYeWrySphQgpNGNr/U+UyUlBkU6n7RAwIgJTz2C+8a8xonZGi6BpSzoQsbVRamr2nlxFDWYNH2j/0=";
-            String timestamp = "1588788367";
-            EventWebhook ew = new EventWebhook();
-            ECPublicKey ellipticCurvePublicKey = ew.ConvertPublicKeyToECDSA(publicKey);
-            boolean valid = ew.VerifySignature(ellipticCurvePublicKey, payload, signature, timestamp);
-            System.out.println("Valid Signature: " + valid);
-        } catch (Exception exception) {
-            Logger.getLogger(Example.class.getName()).log(Level.SEVERE, "something went wrong", exception);
-        }
+        final Route webhookHandler = (req, res) -> {
+            try {
+                final String publicKey = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE83T4O/n84iotIvIW4mdBgQ/7dAfSmpqIM8kF9mN1flpVKS3GRqe62gw+2fNNRaINXvVpiglSI8eNEc6wEA3F+g==";
+
+                final String signature = req.headers(EventWebhookHeader.SIGNATURE.toString());
+                final String timestamp = req.headers(EventWebhookHeader.TIMESTAMP.toString());
+                final byte[] requestBody = req.bodyAsBytes();
+
+                final EventWebhook ew = new EventWebhook();
+                final ECPublicKey ellipticCurvePublicKey = ew.ConvertPublicKeyToECDSA(publicKey);
+                final boolean valid = ew.VerifySignature(ellipticCurvePublicKey, requestBody, signature, timestamp);
+                System.out.println("Valid Signature: " + valid);
+
+                if (valid) {
+                    res.status(204);
+                } else {
+                    res.status(403);
+                }
+
+                return null;
+            } catch (final Exception exception) {
+                res.status(500);
+                return exception.toString();
+            }
+        };
+
+        post("/sendgrid/webhook", webhookHandler);
     }
 }
