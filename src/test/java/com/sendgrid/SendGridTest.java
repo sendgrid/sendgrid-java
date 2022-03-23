5015d5343b593080dfb916e29537b6c00982a283
Twilio <team_interfaces+github@twilio.com>
2022-03-23 11:35:53 -0700

    Release 4.9.1

--- src/test/java/com/sendgrid/SendGridTest.java
+++ src/test/java/com/sendgrid/SendGridTest.java
@@ -43,7 +43,7 @@ public class SendGridTest {
   @Test
   public void testLibraryVersion() {
     SendGrid sg = new SendGrid(SENDGRID_API_KEY);
-    Assert.assertEquals(sg.getLibraryVersion(), "4.9.0");
+    Assert.assertEquals(sg.getLibraryVersion(), "4.9.1");
   }
 
   @Test
